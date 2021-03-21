62f22d9c3e92636e2c5723a229c7cce8846e2019
Abby Brown <abby_brown3@baylor.edu>
2021-03-21 16:43:49 -0500

    initial commit

---
+++ nlp_1.py
@@ -0,0 +1,67 @@
+# Through machine learning, trying to teach computers
+##how we communicate and process information
+
+from textblob import TextBlob
+
+'''
+text = "Today is a beautiful day. Tomorrow looks like bad weather."
+
+blob = TextBlob(text)
+
+print(blob)
+
+# print(blob.sentences)  # Prints the separate sentences
+
+# print(blob.words)  # Prints the separate words
+
+# print(blob.tags)  # Nouns, pronouns, verbs, etc.
+
+# print(blob.noun_phrases)
+
+print(blob.sentiment)  # Module has been trained on movie reviews
+# Closer to 1 = more subjective
+# Closer to 0 = more neutral (polarity)
+
+print(round(blob.sentiment.polarity, 3))
+
+print(round(blob.sentiment.subjectivity, 3))
+
+"""sentences = blob.sentences
+for sentence in sentences:
+    print(sentence)
+    print(sentence.sentiment)
+    print(round(sentence.sentiment.polarity, 3))"""
+
+from textblob.sentiments import NaiveBayesAnalyzer
+
+blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+print(blob.sentiment)
+
+for sentence in blob.sentences:
+    print(sentence.sentiment)
+
+blob.detect_language()
+
+spanish = blob.translate(to="es")
+
+print(spanish)'''
+
+
+from textblob import Word
+
+index = Word("index")
+print(index.pluralize())
+
+cacti = Word("cacti")
+print(cacti.singularize())
+
+word = Word("theyr")
+print(word.spellcheck())
+corrected_word = word.correct()
+
+sentence = TextBlob("Ths sentense has mispeled wrds.")
+corrected_sentence = sentence.correct()
+
+
+print(corrected_sentence)
+print(corrected_word)
