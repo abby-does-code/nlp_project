2233fe770fb07cdecd6a6d1697680c05d0b47e7f
Abby Brown <abby_brown3@baylor.edu>
2021-03-22 15:32:57 -0500

    pandas not working

--- nlp_2.py
+++ nlp_2.py
@@ -1,7 +1,7 @@
 from textblob import TextBlob
 import nltk
 
-nltk.download("stopwords")
+# nltk.download("stopwords")
 from pathlib import Path
 from nltk.corpus import stopwords
 
@@ -16,3 +16,10 @@ print(blob.word_counts["romeo"])
 print(blob.word_counts["thou"])
 
 print(blob.words.count("joy"))
+
+# Let's call the word counts items method
+items = blob.word_counts.items()
+print(items)
+
+items = [item for item in items if item[0] not in stops]
+print(items)
