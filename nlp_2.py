d10ceb9a887539f802f0e9ec5f17ff4c27cc6d97
Abby Brown <abby_brown3@baylor.edu>
2021-03-22 15:44:46 -0500

    ugh

--- nlp_2.py
+++ nlp_2.py
@@ -22,4 +22,34 @@ items = blob.word_counts.items()
 print(items)
 
 items = [item for item in items if item[0] not in stops]
-print(items)
+print(items[:10])
+
+# itemgetter is a native python command
+from operator import itemgetter
+
+sorted_items = sorted(items)
+print(sorted_items[:10])
+
+sorted_items = sorted(items, key=itemgetter(1), reverse=True)
+print(sorted_items[:10])
+
+
+stops = stopwords("english")
+
+stops += more_stops
+
+
+# Grab top 20
+top20 = sorted_items[:20]
+
+print(top20)
+
+# Datagrame
+df = pd.DataFrame(top20, columns=["word", "count"])
+print(df)
+
+import matplotlib.pyplot as plt
+
+df.plot.bar(
+    x="word", y="count", rot=0, legend=False, color=["y", "c", "m", "b", "g", "r"]
+)
