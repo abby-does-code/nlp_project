14fe96f52bfc5023c82f4d25f58b15c7e1378a60
Abby Brown <abby_brown3@baylor.edu>
2021-03-27 09:34:29 -0400

    -just taking down code from lecture; can't hear instruction throgug the video

--- lam_1.py
+++ lam_1.py
@@ -1,5 +1,32 @@
-#Lamda functions! 
-    ## Can take any number of arguments, but only one expression.
-    ##Lamda function returns a function object
-    ##You can only have one expression, but as many arguments as you want!
-    
+# Lamda functions!
+## Can take any number of arguments, but only one expression.
+##Lamda function returns a function object
+##You can only have one expression, but as many arguments as you want!
+
+remainder = lambda num: num % 2
+
+# print(remainder(5))
+# OUTPUT: 1
+
+product = lambda x, y: x * y
+
+# print(product(2, 3))
+# OUTPUT: 6
+
+
+def testfunc(num):
+    print(num)
+    return lambda x: x * num
+
+
+result1 = testfunc(10)
+result2 = testfunc(100)
+
+print(result1(9))
+print(result2(9))
+
+"""OUTPUT: 
+10
+100
+90
+900"""
